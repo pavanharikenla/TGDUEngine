514095191d15b1eb55725664c9422a5151898bf2
Administrator <Administrator@scsbwin-978123.ITTPA.EBIZ.VERIZON.COM>
2015-10-29 09:33:09 -0700

    updated class

---
+++ src/main/java/com/vz/tdataom/util/TSVReader.java
@@ -0,0 +1,78 @@
+package com.vz.tdataom.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import com.univocity.parsers.tsv.TsvParser;
+import com.univocity.parsers.tsv.TsvParserSettings;
+
+public class TSVReader {
+
+	public List<String[]> someMethod(){
+		TsvParserSettings settings = new TsvParserSettings();
+		settings.selectIndexes(0, 1, 7);
+	    //the file used in the example uses '\n' as the line separator sequence.
+	    //the line separator sequence is defined here to ensure systems such as MacOS and Windows
+	    //are able to process this file correctly (MacOS uses '\r'; and Windows uses '\r\n').
+	    settings.getFormat().setLineSeparator("\n");
+
+	    // creates a TSV parser
+	    TsvParser parser = new TsvParser(settings);
+	    List<String[]> allRows = null;
+	    // parses all rows in one go.
+	    try {
+			allRows = parser.parseAll(getReader("C:\\Ravi\\SampleTSV.txt"));
+				/*long millisecondsSinceEpoch = Long.parseLong(cpntent[1]);
+				DateTime dt = new DateTime(millisecondsSinceEpoch);//YYY-MM-DD
+				DateTime  dataisageEndTime = dt.plusMinutes(10);
+				// and here's how to get the String representation
+				//final String timeString2 = dt.toString("MM/dd/YYYY HH:mm:ss");
+				
+				System.out.println("Start Time: "+dt);
+				System.out.println("End Time: "+dataisageEndTime);*/
+				
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    return allRows;
+	}
+	
+	/*public void getDate(){
+		
+		long millisecondsSinceEpoch = Long.parseLong(cpntent[1]);
+		
+		
+		DateTime dt = new DateTime(millisecondsSinceEpoch);//YYY-MM-DD
+		DateTime  dataisageEndTime = dt.plusMinutes(10);
+		// and here's how to get the String representation
+		//final String timeString2 = dt.toString("MM/dd/YYYY HH:mm:ss");
+		
+		System.out.println("Start Time: "+dt);
+		System.out.println("End Time: "+dataisageEndTime);
+	}*/
+	public Reader getReader(String relativePath)  {
+        System.out.println("File: "+relativePath+"this calss: "+this.getClass());
+        InputStreamReader isR = null;
+        try {
+        	isR = new InputStreamReader(new FileInputStream(relativePath));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return isR;
+        
+    }
+
+
+}
+
+
