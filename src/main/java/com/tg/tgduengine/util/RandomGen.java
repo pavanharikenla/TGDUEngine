d16c3e9fedb7e31305ba1e4caf5467db0ecae506
Administrator <Administrator@scsbwin-918502.ITTPA.EBIZ.VERIZON.COM>
2015-10-29 23:50:16 -0700

    added new method

--- src/main/java/com/tg/tgduengine/util/RandomGen.java
+++ src/main/java/com/tg/tgduengine/util/RandomGen.java
@@ -4,12 +4,19 @@ public class RandomGen {
 	public static void main(String args[]){
 		RandomGen rg = new RandomGen();
 		rg.mobileNum();		
+		rg.email();				
 	}
 	
 	public long mobileNum(){
 		long number = 0;		
 		number = (long) Math.floor(Math.random() * 9000000000L) + 1000000000L;		
-		System.out.println(number);
 		return number;
+	}
+	
+	public String email(){
+		String email = "default@verixyz.com";		
+		long number = (long) Math.floor(Math.random() * 9000000000L) + 1000000000L;	
+		email = String.valueOf(number) + "@verixyz.com";		
+		return email;
 	}
 }
